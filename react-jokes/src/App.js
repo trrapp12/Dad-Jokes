e82fa24ea34e3c08243a9c12238728a0a1af8765
Trevor Rapp <trrapp@gmail.com>
2022-03-01 00:05:22 -0700

    fix some syntax

--- react-jokes/src/App.js
+++ react-jokes/src/App.js
@@ -1,27 +1,31 @@
 import React from 'react'
+import Jokes from './Jokes'
 
-function App (props) {
+function App () {
   return (
-    <Jokes 
-      setup="I got my daughter a fridge for her birthday."
-      punchline="I can't wait to see her face light up when she opens it."
-    />
-    <Jokes 
-      setup="How did the hacker escape the police?"
-      punchline="He just ransomware!"
-    />
-    <Jokes 
-      setup="Why don't pirates travel on mountain roads?"
-      punchline="Scurvy."
-    />
-    <Jokes 
-      setup="Why do bees stay in the hive in the winter?"
-      punchline="Swarm."
-    />
-    <Jokes 
-      setup="What's the best thing about Switzerland?"
-      punchline="I don't know, but the flag is a big plus!"
-    />
+    <>
+      <Jokes 
+        setup="I got my daughter a fridge for her birthday."
+        punchline="I can't wait to see her face light up when she opens it."
+      />
+      <Jokes 
+        setup="How did the hacker escape the police?"
+        punchline="He just ransomware!"
+      />
+      <Jokes 
+        setup="Why don't pirates travel on mountain roads?"
+        punchline="Scurvy."
+      />
+      <Jokes 
+        setup="Why do bees stay in the hive in the winter?"
+        punchline="Swarm."
+      />
+      <Jokes 
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is a big plus!"
+      />
+    </>
+
   )
 }
 
