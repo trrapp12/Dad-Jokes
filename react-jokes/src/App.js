eb567e9ffd1e84958cb391ff71ea7a857ca3c948
Trevor Rapp <trrapp@gmail.com>
2022-03-02 12:53:57 -0700

    add signature line

--- react-jokes/src/App.js
+++ react-jokes/src/App.js
@@ -7,6 +7,10 @@ function App () {
       <div className="p-container">
         <h1> React Props Practice: React Jokes</h1>
         <p>This is a project designed to practice using props in React.  For a look at what's going on under the hood, please see the GitHub repository at <a href="https://github.com/trrapp12/React-Jokes">React-Jokes</a>.  And while you're waiting, hover over the jokes to reveal the punchline!</p>
+        <br></br>
+        <p>Cheers,</p>
+        <br></br>
+        <p>T.R.Rapp</p>
       </div>
       <Jokes 
         setup="I got my daughter a fridge for her birthday."
