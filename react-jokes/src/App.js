8803a47d320744ae5999955020aa7ec6e1c1632d
Trevor Rapp <trrapp@gmail.com>
2022-03-02 06:20:58 -0700

    add build instructions to package.json for static build in Github

--- react-jokes/src/App.js
+++ react-jokes/src/App.js
@@ -3,7 +3,8 @@ import Jokes from './Jokes'
 
 function App () {
   return (
-    <>
+    <div>
+      <p>This is a project designed to practice using props in React.  For a look at what's going on under the hood, please see the GitHub repository at<a href="https://github.com/trrapp12/React-Jokes">React-Jokes</a>.  And while you're waiting, hover over the jokes to reveal the punchline!</p>
       <Jokes 
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
@@ -24,7 +25,7 @@ function App () {
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
       />
-    </>
+    </div>
 
   )
 }
--- react-jokes/src/Jokes.js
+++ react-jokes/src/Jokes.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Jokes(props) {
   return (
-    <>
+    <div className="container">
       <div className="setup">{props.setup}</div>
       <div className="punchline">{props.punchline}</div>
-    </>
+    </div>
 
   )
 }
