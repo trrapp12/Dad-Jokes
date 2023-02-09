7fee1c0d327f1fb2e4d4f3c3fc2653f8579d01e8
Trevor Rapp <trrapp@gmail.com>
2023-02-08 23:16:14 -0700

    redsign of colors and adds randInt for making jokes random

--- react-jokes/src/App.js
+++ react-jokes/src/App.js
@@ -6,17 +6,24 @@ function App () {
   const jokesArray = jokesData.map((value) => {
     return <Jokes setup={value.setup} punchline={value.punchline} rating={value.rating} attribution={value.attribution} />
   })
+
+
+  let randInt = Math.floor(Math.random() * jokesArray.length) + 1
+
+  console.log(randInt)
+
   return (
-    <div>
+    <div className="master-container">
+      <div className="circle"></div>
+      <h1> Dad Jokes</h1>
       <div className="p-container">
-        <h1> React Props Practice: React Jokes</h1>
-        <p>This is a project designed to practice using props in React.  For a look at what's going on under the hood, please see the GitHub repository at <a href="https://github.com/trrapp12/React-Jokes">React-Jokes</a>.  And while you're waiting, hover over the jokes to reveal the punchline!</p>
+        <p>This is a project demonstrates passing props in React.  See the code at <a href="https://github.com/trrapp12/React-Jokes">React-Jokes</a>.  And while you're waiting, enjoy a good Dad joke!</p>
         <br></br>
         <p>Cheers,</p>
         <br></br>
         <p>T.R.Rapp</p>
       </div>
-      {jokesArray}
+      {jokesArray[randInt]}
     </div>
 
   )
